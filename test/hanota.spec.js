fa780ff46790448a137229b3213ce808b27c4eb3
luffy <luffybryant@gmail.com>
2020-05-26 00:15:55 +0800

    添加12-整数转罗马数字

--- test/hanota.spec.js
+++ test/hanota.spec.js
@@ -2,13 +2,13 @@ import hanota from '../src/hanota'
 import { expect } from 'chai'
 
 describe('hanota', () => {
-    it('hanota test A = [2, 1, 0], B = [], C = []', () => {
+    it('test A = [2, 1, 0], B = [], C = []', () => {
         let A = [2, 1, 0], B = [], C = []
         let res = hanota(A, B, C)
         expect(JSON.stringify(res)).to.equal(JSON.stringify([2, 1, 0]))
     })
 
-    it('hanota test A = [1, 0], B = [], C = []', () => {
+    it('test A = [1, 0], B = [], C = []', () => {
         let A = [1, 0], B = [], C = []
         let res = hanota(A, B, C)
         expect(JSON.stringify(res)).to.equal(JSON.stringify([1, 0]))
