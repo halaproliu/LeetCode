520f09168c8ded5aad4c9f7229270694fd95a600
luffy <luffybryant@gmail.com>
2020-05-30 19:52:00 +0800

    更新16-最接近的三数之和

--- src/threeSumClosest.js
+++ src/threeSumClosest.js
@@ -5,31 +5,29 @@
  */
 var threeSumClosest = function (nums, target) {
     let l, r, diff, sum = 0, ans
+    let len = nums.length
+    if (len < 3) return null
     // 先进行排序
     nums.sort((a, b) => a - b)
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < len; i++) {
         l = i + 1
-        r = nums.length - 1
+        r = len - 1
         while (l < r) {
             // 求和
             sum = nums[i] + nums[l] + nums[r]
-            // 获取当前和于目标值的差值
-            let currDiff = Math.abs(sum - target)
-            if (currDiff === 0) return sum
-            if (!diff) {
-                diff = currDiff
-                ans = sum
-            }
-            if (sum < target) {
-                if (nums[l] === nums[l + 1]) l++
+            if (sum === target) {
+                return sum
+            } else if (sum < target) {
+                while (nums[l] === nums[l + 1]) l++
                 l++
-            } else if (sum > target) {
-                r--
             } else {
-                return ans
+                while (nums[r] === nums[r - 1]) r--
+                r--
             }
 
-            if (currDiff < diff) {
+            // 获取当前和于目标值的差值
+            let currDiff = Math.abs(sum - target)
+            if (!diff || currDiff < diff) {
                 diff = currDiff
                 ans = sum
             }
