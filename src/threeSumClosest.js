e885f02e6793b0fb24bf3378c61a03b00c10b8df
刘文剑(金服银行科技中心消费金融产品系统研发团队渠道系统研发组) <liuwenjian091@pingan.com.cn>
2020-05-30 17:18:50 +0800

    添加16-最接近的三数之和

---
+++ src/threeSumClosest.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function (nums, target) {
+    let l, r, diff, sum = 0, ans
+    // 先进行排序
+    nums.sort((a, b) => a - b)
+    for (let i = 0; i < nums.length; i++) {
+        l = i + 1
+        r = nums.length - 1
+        while (l < r) {
+            // 求和
+            sum = nums[i] + nums[l] + nums[r]
+            // 获取当前和于目标值的差值
+            let currDiff = Math.abs(sum - target)
+            if (currDiff === 0) return sum
+            if (!diff) {
+                diff = currDiff
+                ans = sum
+            }
+            if (sum < target) {
+                if (nums[l] === nums[l + 1]) l++
+                l++
+            } else if (sum > target) {
+                r--
+            } else {
+                return ans
+            }
+
+            if (currDiff < diff) {
+                diff = currDiff
+                ans = sum
+            }
+        }
+    }
+    return ans
+};
+
+let a = threeSumClosest([1, 1, -1, -1, 3], -1)
+console.log(a)
+
+export default threeSumClosest
